b3a815c4d923f43540fbe8f4846a5478783e08d2
Stanislav <staspovolotskiy@gmail.com>
2020-03-26 10:41:23 +0300

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Stanislav Povolotskiy on 26.03.2020.
+//  Copyright © 2020 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
